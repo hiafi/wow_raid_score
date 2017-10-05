151afbeea89fcd8b791932786f393807a694a5c3
Jonathan Roberts <hiafi3@gmail.com>
2017-10-05 13:54:20 -0700

    Added a way to get the status

--- WoWRaidScore/urls.py
+++ WoWRaidScore/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^parse\/status\/(?P<raid_id>\w+)\/$', views.view_parse_progress),
     url(r'^parse\/(?P<raid_id>\w+)\/$', views.parse_raid),
     url(r'^parse_legacy\/(?P<raid_id>\w+)\/$', views.parse_raid_legacy),
     url(r'^(?P<raid_id>\w+)\/(?P<player_id>[0-9]+)\/(?P<boss_id>[0-9]+)\/$', views.view_player_details_for_raid),

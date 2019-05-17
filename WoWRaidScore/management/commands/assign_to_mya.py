c2ffea5e73cc52efe076ba226aeb2ffb17e13a63
Jonathan Roberts <hiafi3@gmail.com>
2019-05-17 00:40:39 -0700

    helper function

---
+++ WoWRaidScore/management/commands/assign_to_mya.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from WoWRaidScore.models import Raid, Group
+from datetime import datetime, timedelta
+
+
+class Command(BaseCommand):
+    help = 'Assign all of the raids to MYA'
+
+    def handle(self, *args, **options):
+        raids = Raid.objects.all()
+        group = Group.objects.get(id=1)
+        for raid in raids:
+            raid.group = group
